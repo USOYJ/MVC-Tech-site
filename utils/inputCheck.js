7f2bb900183f9e2e301ac460d73f1802835bc961
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-04 00:55:19 -0400

    seeds

--- utils/inputCheck.js
+++
@@ -1,9 +0,0 @@
-const checkInputs = (user_name, password) => {
-    if (user_name == '') {
-        alert('Username');
-    } if (password == '') {
-        alert('Password');
-    }
-}
-
-module.exports = checkInputs;
