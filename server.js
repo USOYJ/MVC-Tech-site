e2b6deef9d83a2b868e02b1e0bf487760cf07f7c
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-04 01:13:14 -0400

    package.json

--- server.js
+++ server.js
@@ -1,38 +1,54 @@
-const helpers = require('./utils/helpers'); 
-const express = require('express'); 
+const express = require('express');
 const path = require('path');
-const exphbs = require('express-handlebars'); 
-const routes = require('./controllers'); 
+const exphbs = require('express-handlebars');
 const session = require('express-session');
-const sequelize = require('../MVC-Tech-site/config/connection'); 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+
+const routes = require('./controllers');
+const sequelize = require('../MVC-Tech-site/config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({helpers});
+
 
 const sess = {
   secret: 'Super oyin secret',
-  cookie: {},
+  cookie: {
+    maxAge: 86400000, 
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
+
 
 app.use(session(sess));
 app.use(express.urlencoded({ extended: true }));
-app.engine('handlebars', hbs.engine);
 app.use(express.json());
+
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 app.use(routes);
 
-sequelize.sync();
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+sequelize.sync({ force: false }) 
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error syncing database:', err);
+  });
+
