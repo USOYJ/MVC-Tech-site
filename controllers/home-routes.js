324c5dd8fbad13460f0271413493d44523aa9113
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-09 12:20:57 -0400

    home

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -73,7 +73,7 @@ router.get("/login", (req, res) => {
   res.render("login");
 });
 
-router.get("/signUp", (req, res) => {
+router.get("/signup", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
