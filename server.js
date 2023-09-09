0fa57c48b4cfdfc2801942d24609500dd819ecd2
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-09 09:17:32 -0400

    heroku wan kill me

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
-const sequelize = require('../MVC-Tech-site/config/connection');
+const sequelize = require("./config/connection");
+
 
 
 const app = express();
